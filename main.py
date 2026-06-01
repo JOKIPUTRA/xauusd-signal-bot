bdedb4f04ae25f8d597e8c6a928ba27b7cdbd2c2
JOKIPUTRA <122268501+JOKIPUTRA@users.noreply.github.com>
2026-06-01 23:16:38 +0700

    Implement market checking and messaging functionality

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import schedule
+import time
 
+from oanda_client import get_xau_price
+from telegram_bot import send_message
+
+def check_market():
+
+    price = get_xau_price()
+
+    if not price:
+        return
+
+    msg = f"""
+XAUUSD LIVE
+
+Bid : {price['bid']}
+Ask : {price['ask']}
+"""
+
+    send_message(msg)
+
+schedule.every(1).minutes.do(check_market)
+
+send_message("Bot XAUUSD berhasil dijalankan")
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
